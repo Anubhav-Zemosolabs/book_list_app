165fc8b72407523676555d81b8f0bf12a22aa8c5
Anubhav-Zemosolabs <anubhav.gupta@zemosolabs.com>
2022-06-20 11:15:26 +0530

    do changes in BookTest File

--- src/test/java/com/bookportal/app/models/BookTest.java
+++ src/test/java/com/bookportal/app/models/BookTest.java
@@ -9,14 +9,14 @@ class BookTest {
     @Test
     public void testEqualsObject(){
         final Book itemA=new Book();
-        final Book itemB=new Book(1,"Book 1","Author 1", 450, "A giant named Ha-grid, who is carrying a bundle of blankets with the baby Harry inside, then falls out of the sky on a motorcycle.", 3.90);
-        final Book itemC=new Book(1,"Book 2","Author 2", 400, "Five point someone is a book that talks about the whole IIT system and what actually students face through in their college life", 4.50);
+        final Book itemB=new Book(1,"Book 1","Author 1", 450, "A giant named Ha-grid, who is carrying a bundle of blankets.", 3.90);
+        final Book itemC=new Book(1,"Book 2","Author 2", 400, "Five point someone is a book that talks about the whole IIT.", 4.50);
         final Object nonObject=new Object();
-     //   Assertions.assertNotEquals(itemA,itemA);
-//        Assertions.assertNotEquals(itemB,itemB);
-//        Assertions.assertNotEquals(itemC,itemB); //id cannot be same as it is unique
-//        Assertions.assertNotEquals(itemA, itemB);
-//        Assertions.assertNotEquals(itemA,nonObject);
+        Assertions.assertTrue(itemA.equals(itemA));
+        Assertions.assertTrue(itemB.equals(itemB));
+        Assertions.assertFalse(itemC.equals(itemB)); //id cannot be same as it is unique
+        Assertions.assertFalse(itemA.equals(itemB));
+        Assertions.assertFalse(itemA.equals(nonObject));
     }
 
     @Test
